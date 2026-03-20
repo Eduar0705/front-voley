ddd3b85d9f4378974d523930440183fb450c2fbf
unknown <eduarjosesuarezgraterol@gmail.com>
2026-03-19 22:07:54 -0400

    API

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = "https://backvoley.up.railway.app/api";
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://backvoley.up.railway.app/api";
+//const API_URL = "http://localhost:3000/api";
 
 
 const api = axios.create({
